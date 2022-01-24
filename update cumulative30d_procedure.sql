e2df6bece1de64b05081d2619d5c7a36f198ec00
Farid Maharramov <97797992+farmak87@users.noreply.github.com>
2022-01-24 16:44:31 +0400

    Add files via upload

---
+++ update cumulative30d_procedure.sql
@@ -0,0 +1,18 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `update_cumulativeTopUP30`()
+BEGIN
+	
+    WITH cte AS 
+    (
+		SELECT CUSTOMERID, sum(RECHARGEAMOUNT) AS CUMULLATIVE30
+		FROM recharges
+		WHERE RECHARGEDATETIME >= date_add(rechargedatetime, interval -30 day)
+		GROUP BY CUSTOMERID
+    )
+	UPDATE customers as c
+	inner join cte on (c.CUSTOMERID = cte.CUSTOMERID)
+	SET c.CUMULATIVE30DAYS  = cte.CUMULLATIVE30
+;    
+END
+    
+    
+    
